7a2655b88d3086f0168474b8c2f3472dedf12b55
lv <1967798998@qq.com>
2025-02-05 01:13:36 +0800

    fix:优化部分问题

--- config_test.go
+++ config_test.go
@@ -28,8 +28,8 @@ func TestTPConfig_NewTP1Config(t *testing.T) {
 	res, err2 := cf.ApiRequest("/v5/ec/query/sim-basic-info", &SimBasicInfoReq{
 		Transid: cf.TransID,
 		Token:   token,
-		Msisdn:  "1442161864994", // 可选字段
-		//Iccid:   "89860846162470274998", // 可选字段
+		//Msisdn:  "1442161864994", // 可选字段
+		Iccid: "89860846162470274998", // 可选字段
 		//Imsi:    "460240261864998",      // 可选字段
 	})
 	if err2 != nil {
@@ -103,27 +103,12 @@ func TestNewTP4Config(t *testing.T) {
 	fmt.Println(request.Token)
 	token := request.Token
 	cf = NewConfig("https://api.iot.10086.cn", "", "")
-	//res, err2 := cf.ApiRequest("/v5/ec/query/sim-basic-info", &SimBasicInfoReq{
-	//	Transid: cf.TransID,
-	//	Token:   token,
-	//	Msisdn:  "1441649449990", // 可选字段
-	//	//Iccid:   "89860846162470274998", // 可选字段
-	//	//Imsi:    "460240261864998",      // 可选字段
-	//})
-	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-real-name-status", &SimBasicInfoReq{
-	//	Transid: cf.TransID,
-	//	Token:   token,
-	//	//Msisdn:  "1442078069363", // 可选字段
-	//	Iccid: "898608301724D0252275", // 可选字段
-	//	//Iccid: "89860837132490570000", // 可选字段
-	//	//Imsi: "1442078067002", // 可选字段
-	//})
-	res1, err2 := cf.ApiRequest("/v5/ec/query/card-bind-status", &SimBasicInfoReq{
+	res2, err2 := cf.ApiRequest("/v5/ec/query/sim-status", &SimBasicInfoReq{
 		Transid:  cf.TransID,
 		Token:    token,
-		TestType: "0",
-		Msisdn:   "1442000252275", // 可选字段
-		//Iccid: "89860837132490563908", // 可选字段
+		OperType: "0",
+		//Msisdn:   "1442078069363", // 可选字段
+		Iccid: "89860814172480061846", // 可选字段
 		//Iccid: "89860837132490570000", // 可选字段
 		//Imsi: "1442078067002", // 可选字段
 	})
@@ -131,5 +116,26 @@ func TestNewTP4Config(t *testing.T) {
 		fmt.Printf(err2.Error())
 		return
 	}
-	fmt.Println(res1)
+	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-real-name-status", &SimBasicInfoReq{
+	//	Transid: cf.TransID,
+	//	Token:   token,
+	//	//Msisdn:  "1442078069363", // 可选字段
+	//	Iccid: "89860814172480062979", // 可选字段
+	//	//Iccid: "89860837132490570000", // 可选字段
+	//	//Imsi: "1442078067002", // 可选字段
+	//})
+	//res1, err2 := cf.ApiRequest("/v5/ec/query/card-bind-status", &SimBasicInfoReq{
+	//	Transid:  cf.TransID,
+	//	Token:    token,
+	//	TestType: "0",
+	//	Msisdn:   "1442000252275", // 可选字段
+	//	//Iccid: "89860837132490563908", // 可选字段
+	//	//Iccid: "89860837132490570000", // 可选字段
+	//	//Imsi: "1442078067002", // 可选字段
+	//})
+	if err2 != nil {
+		fmt.Printf(err2.Error())
+		return
+	}
+	fmt.Println(res2)
 }
--- testData/main.go
+++ testData/main.go
@@ -1,51 +1,52 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/dmlvhh/mobile_sdk"
+	"net/url"
 )
 
-//func main() {
-//	Tp := mobile_sdk.NewTP1Config("http://token.dctxiot.com/api/token/getToken", "29030", "a0674936bd251655ff8e14e18c74b879", "3")
-//	params := url.Values{}
-//	params.Add("appid", Tp.AppID)
-//	params.Add("app_secret", Tp.AppSecret)
-//	params.Add("channel_id", Tp.ChannelID)
-//	params.Add("times", Tp.Times)
-//	params.Add("sign", Tp.Sign)
-//	request, err := Tp.ApiGetRequest(params)
-//	if err != nil {
-//		return
-//	}
-//	//fmt.Println(request.Data.Token)
-//	token := request.Data.Token
-//	cf := mobile_sdk.NewConfig("https://api.iot.10086.cn", "29030", "a0674936bd251655ff8e14e18c74b879")
-//	//res, err2 := cf.ApiRequest("/v5/ec/query/sim-basic-info", &mobile_sdk.SimBasicInfoReq{
-//	//	Transid: cf.TransID,
-//	//	Token:   token,
-//	//	//Msisdn:  "1442161864994", // 可选字段
-//	//	Iccid: "89860846162470274994", // 可选字段
-//	//	//Imsi:    "460240261864998",      // 可选字段
-//	//})
-//	//if err2 != nil {
-//	//	fmt.Printf(err2.Error())
-//	//	return
-//	//}
-//	//res, err := cf.SimRealNameStatus(&mobile_sdk.SimBasicInfoReq{
-//	//	Transid: cf.TransID,
-//	//	Token:   token,
-//	//	Iccid:   "89860846162470274994",
-//	//})
-//	res, err := cf.CardBindStatus(&mobile_sdk.SimBasicInfoReq{
-//		Transid:  cf.TransID,
-//		Token:    token,
-//		TestType: "0",
-//		//Iccid:    "89860846162470274994",
-//		Msisdn: "1442161864994", // 可选字段
-//	})
-//	fmt.Println(res.Result)
-//}
+func main() {
+	//Tp := mobile_sdk.NewTP1Config("http://token.dctxiot.com/api/token/getToken", "29030", "a0674936bd251655ff8e14e18c74b879", "3")
+	Tp := mobile_sdk.NewTP1Config("http://token.dctxiot.com/api/token/getToken", "29030", "a0674936bd251655ff8e14e18c74b879", "8")
+	params := url.Values{}
+	params.Add("appid", Tp.AppID)
+	params.Add("app_secret", Tp.AppSecret)
+	params.Add("channel_id", Tp.ChannelID)
+	params.Add("times", Tp.Times)
+	params.Add("sign", Tp.Sign)
+	request, err := Tp.ApiGetRequest(params)
+	if err != nil {
+		return
+	}
+	fmt.Println(request.Data.Token)
+	//token := request.Data.Token
+	//cf := mobile_sdk.NewConfig("https://api.iot.10086.cn", "29030", "a0674936bd251655ff8e14e18c74b879")
+	//res, err2 := cf.ApiRequest("/v5/ec/query/sim-basic-info", &mobile_sdk.SimBasicInfoReq{
+	//	Transid: cf.TransID,
+	//	Token:   token,
+	//	//Msisdn:  "1442161864994", // 可选字段
+	//	Iccid: "89860846162470274994", // 可选字段
+	//	//Imsi:    "460240261864998",      // 可选字段
+	//})
+	//if err2 != nil {
+	//	fmt.Printf(err2.Error())
+	//	return
+	//}
+	//res, err := cf.SimRealNameStatus(&mobile_sdk.SimBasicInfoReq{
+	//	Transid: cf.TransID,
+	//	Token:   token,
+	//	Iccid:   "89860846162470274994",
+	//})
+	//res, err := cf.CardBindStatus(&mobile_sdk.SimBasicInfoReq{
+	//	Transid:  cf.TransID,
+	//	Token:    token,
+	//	TestType: "0",
+	//	//Iccid:    "89860846162470274994",
+	//	Msisdn: "1442161864994", // 可选字段
+	//})
+	//fmt.Println(res.Result)
+}
 
 //func main() {
 //	Tp := mobile_sdk.NewTP2Config("https://ac.buleideiot.com/api/get-chinamobile-onelink-token", "sj4xACNePCXgZARj", "22")
@@ -99,82 +100,82 @@ import (
 //	fmt.Println(res)
 //}
 
-func main() {
-	Tp := mobile_sdk.NewTP3Config("http://www.llk.10086link.cn/web/api/iot/yijia/get/cmcc/token", "CMCC888888888888103", "1868309673338204160")
-
-	request, err := Tp.ApiPostRequest(map[string]string{
-		"appId":     Tp.AppID,
-		"accountId": Tp.AccountID,
-	})
-	if err != nil {
-		return
-	}
-	var res mobile_sdk.TP3TokenRes
-	json.Unmarshal([]byte(request), &res)
-	fmt.Println(res)
-	cf := mobile_sdk.NewConfig("https://api.iot.10086.cn", "", "")
-	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-basic-info", &mobile_sdk.SimBasicInfoReq{
-	//	Transid: cf.TransID,
-	//	Token:   res.Data,
-	//	//Msisdn:  "1442078069363", // 可选字段
-	//	//Iccid: "89860837132490489025", // 可选字段
-	//	Iccid: "89860837132490570000", // 可选字段
-	//	//Imsi:    "460240261864998",      // 可选字段
-	//})
-	res1, err2 := cf.ApiRequest("/v5/ec/query/sim-real-name-status", &mobile_sdk.SimBasicInfoReq{
-		Transid: cf.TransID,
-		Token:   res.Data,
-		//Msisdn:  "1442078069363", // 可选字段
-		Iccid: "89860837132490563909", // 可选字段
-		//Iccid: "89860837132490570000", // 可选字段
-		//Imsi: "1442078067002", // 可选字段
-	})
-	if err2 != nil {
-		fmt.Printf(err2.Error())
-		return
-	}
-	fmt.Println(res1)
-	//res1, err2 := cf.ApiRequest("/v5/ec/query/card-bind-status", &mobile_sdk.SimBasicInfoReq{
-	//	Transid:  cf.TransID,
-	//	Token:    res.Data,
-	//	TestType: "0",
-	//	Msisdn:   "1442078069363", // 可选字段
-	//	//Iccid: "89860837132490563908", // 可选字段
-	//	//Iccid: "89860837132490570000", // 可选字段
-	//	//Imsi: "1442078067002", // 可选字段
-	//})
-	//if err2 != nil {
-	//	fmt.Printf(err2.Error())
-	//	return
-	//}
-	//fmt.Println(res1)
-
-	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-status", &mobile_sdk.SimBasicInfoReq{
-	//	Transid:  cf.TransID,
-	//	Token:    res.Data,
-	//	OperType: "0",
-	//	//Msisdn:   "1442078069363", // 可选字段
-	//	Iccid: "89860837132490567358", // 可选字段
-	//	//Iccid: "89860837132490570000", // 可选字段
-	//	//Imsi: "1442078067002", // 可选字段
-	//})
-	//if err2 != nil {
-	//	fmt.Printf(err2.Error())
-	//	return
-	//}
-	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-data-usage", &mobile_sdk.SimBasicInfoReq{
-	//	Transid:  cf.TransID,
-	//	Token:    res.Data,
-	//	OperType: "0",
-	//	//Msisdn:   "1442078069363", // 可选字段
-	//	Iccid: "89860837132490495120", // 可选字段
-	//	//Iccid: "89860837132490570000", // 可选字段
-	//	//Imsi: "1442078067002", // 可选字段
-	//})
-	//if err2 != nil {
-	//	fmt.Printf(err2.Error())
-	//	return
-	//}
-	//fmt.Println(res1)
-
-}
+//func main() {
+//	Tp := mobile_sdk.NewTP3Config("http://www.llk.10086link.cn/web/api/iot/yijia/get/cmcc/token", "CMCC888888888888103", "1868309673338204160")
+//
+//	request, err := Tp.ApiPostRequest(map[string]string{
+//		"appId":     Tp.AppID,
+//		"accountId": Tp.AccountID,
+//	})
+//	if err != nil {
+//		return
+//	}
+//	var res mobile_sdk.TP3TokenRes
+//	json.Unmarshal([]byte(request), &res)
+//	fmt.Println(res.Data)
+//	cf := mobile_sdk.NewConfig("https://api.iot.10086.cn", "", "")
+//	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-basic-info", &mobile_sdk.SimBasicInfoReq{
+//	//	Transid: cf.TransID,
+//	//	Token:   res.Data,
+//	//	//Msisdn:  "1442078069363", // 可选字段
+//	//	//Iccid: "89860837132490489025", // 可选字段
+//	//	Iccid: "89860837132490570000", // 可选字段
+//	//	//Imsi:    "460240261864998",      // 可选字段
+//	//})
+//	res1, err2 := cf.ApiRequest("/v5/ec/query/sim-real-name-status", &mobile_sdk.SimBasicInfoReq{
+//		Transid: cf.TransID,
+//		Token:   res.Data,
+//		//Msisdn:  "1442078069363", // 可选字段
+//		Iccid: "89860837132490563909", // 可选字段
+//		//Iccid: "89860837132490570000", // 可选字段
+//		//Imsi: "1442078067002", // 可选字段
+//	})
+//	if err2 != nil {
+//		fmt.Printf(err2.Error())
+//		return
+//	}
+//	fmt.Println(res1)
+//	//res1, err2 := cf.ApiRequest("/v5/ec/query/card-bind-status", &mobile_sdk.SimBasicInfoReq{
+//	//	Transid:  cf.TransID,
+//	//	Token:    res.Data,
+//	//	TestType: "0",
+//	//	Msisdn:   "1442078069363", // 可选字段
+//	//	//Iccid: "89860837132490563908", // 可选字段
+//	//	//Iccid: "89860837132490570000", // 可选字段
+//	//	//Imsi: "1442078067002", // 可选字段
+//	//})
+//	//if err2 != nil {
+//	//	fmt.Printf(err2.Error())
+//	//	return
+//	//}
+//	//fmt.Println(res1)
+//
+//	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-status", &mobile_sdk.SimBasicInfoReq{
+//	//	Transid:  cf.TransID,
+//	//	Token:    res.Data,
+//	//	OperType: "0",
+//	//	//Msisdn:   "1442078069363", // 可选字段
+//	//	Iccid: "89860837132490567358", // 可选字段
+//	//	//Iccid: "89860837132490570000", // 可选字段
+//	//	//Imsi: "1442078067002", // 可选字段
+//	//})
+//	//if err2 != nil {
+//	//	fmt.Printf(err2.Error())
+//	//	return
+//	//}
+//	//res1, err2 := cf.ApiRequest("/v5/ec/query/sim-data-usage", &mobile_sdk.SimBasicInfoReq{
+//	//	Transid:  cf.TransID,
+//	//	Token:    res.Data,
+//	//	OperType: "0",
+//	//	//Msisdn:   "1442078069363", // 可选字段
+//	//	Iccid: "89860837132490495120", // 可选字段
+//	//	//Iccid: "89860837132490570000", // 可选字段
+//	//	//Imsi: "1442078067002", // 可选字段
+//	//})
+//	//if err2 != nil {
+//	//	fmt.Printf(err2.Error())
+//	//	return
+//	//}
+//	//fmt.Println(res1)
+//
+//}
